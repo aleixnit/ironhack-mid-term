e0f63be0bb73b735b539bef70eacf326aa13f73d
Aleix <nitjac@gmail.com>
2023-01-27 13:12:43 +0100

    menu hamburguer terminado

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,9 @@
 /* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
+const hamburger = document.querySelector(".hamburgerMenu")
+const navMenu = document.querySelector(".menuList")
+
+hamburger.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("active");
+    menuList.classList.toggle("active");
+
+})
