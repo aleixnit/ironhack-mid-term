477d422da1f807d8d91862d0d6b298b9004b7b9a
Aleix <nitjac@gmail.com>
2023-01-27 05:08:59 +0100

    contact us acabado html y javascript

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -1,2 +1,30 @@
-/* Crea tu propia lógica para hacer un fetch que emule una post request a un servidor y enseñe un mensaje en consola cuando la llamada se resuelva */
-/*  ADVANCED: utiliza DOM manipulation para enseñarle al user que su mensaje se ha enviado correctamente o no */
+let submit = (e) => {
+    // Verificacion y "refrescar" conceptos de semana pasada
+    e.preventDefault();
+    console.log(e);
+    console.log("CTA Connected");
+    // Vamos a traernos el valor de los inputs del html
+    let yourName = document.querySelector("#yourName").value;
+    let yourEmail = document.querySelector("#yourEmail").value;
+    let yourPhone = document.querySelector("#yourPhone").value;
+    let yourMessage = document.querySelector("#yourMessage").value;
+  
+  
+    // Uso de fetch para empujar valores del title y el body del formulario al api PUBLICO externo
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        name: yourName,
+        email: yourEmail,
+        phone: yourPhone,
+        message: yourMessage
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  };
+  
+  document.querySelector(".form-boton").addEventListener("click", submit);
